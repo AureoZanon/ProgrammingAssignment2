be663d8f791c2b82eb9b930bda1c9b6278ec5a7d
AureoZanon <AureoZanon@users.noreply.github.com>
2014-05-25 16:26:45 -0500

    Update cachematrix.R
    This is my response for the Programming Assignment 2 of the R Programming course of the Coursera's Data Science specialization. The functions makeCacheMatrix.R and cacheSolve.R passed the test in R Studio and were saved in the same repository.

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,22 @@
 ## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+          ## initialization
           z<-NULL
           set<-function(y){
                     x<<-y
                     z<<-NULL
           }
+          
+          ## get function
           get<-function() x
+          
+          ## setting the matrix
           setmatrix <- function(solve) z <<- solve
+          
+          ## getting the matrix
           getmatrix <- function() z
+          
           list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 }
 
@@ -22,11 +30,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x=matrix(), ...) {
           z<-x$getmatrix()
+          
+          ## checking and getting data from the chache
           if(!is.null(z)){
                     message("getting data from the cache")
                     return(z)
           }
+          
           matrix<-x$get()
+          
+          ## matrix inversion using function "solve"
           z<-solve(matrix, ...)
           x$setmatrix(z)
           z
